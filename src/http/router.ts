f8715b977c6b08a026d049a50021831cc22040d8
Michael L Perry <michael@qedcode.com>
2022-09-04 14:17:49 -0500

    Return HTTP 201 Created from /save and /write

--- src/http/router.ts
+++ src/http/router.ts
@@ -106,6 +106,65 @@ function postString<U>(method: (user: RequestUser, message: string) => Promise<U
     };
 }
 
+function postCreate<T>(method: (user: RequestUser, message: T) => Promise<void>): Handler {
+    return (req, res, next) => {
+        const user = <RequestUser>req.user;
+        const message = <T>req.body;
+        if (!message) {
+            throw new Error('Ensure that you have called app.use(express.json()).');
+        }
+        method(user, message)
+            .then(_ => {
+                res.sendStatus(201);
+                next();
+            })
+            .catch(error => {
+                if (error instanceof Forbidden) {
+                    Trace.warn(error.message);
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.status(403).send(error.message);
+                }
+                else {
+                    Trace.error(error);
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.sendStatus(500).send(error.message);
+                }
+                next();
+            });
+    };
+}
+
+function postStringCreate(method: (user: RequestUser, message: string) => Promise<void>): Handler {
+    return (req, res, next) => {
+        const user = <RequestUser>req.user;
+        const input = <string>req.body;
+        if (!input || typeof(input) !== 'string') {
+            res.setHeader('Content-Type', 'text/plain');
+            res.status(500).send('Expected Content-Type text/plain. Ensure that you have called app.use(express.text()).');
+        }
+        else {
+            method(user, input)
+                .then(_ => {
+                    res.sendStatus(201);
+                    next();
+                })
+                .catch(error => {
+                    if (error instanceof Forbidden) {
+                        Trace.warn(error.message);
+                        res.setHeader('Content-Type', 'text/plain');
+                        res.status(403).send(error.message);
+                    }
+                    else {
+                        Trace.error(error);
+                        res.setHeader('Content-Type', 'text/plain');
+                        res.status(400).send(error.message);
+                    }
+                    next();
+                });
+        }
+    };
+}
+
 function serializeUserIdentity(user: RequestUser) : UserIdentity {
     if (!user) {
         return null;
@@ -130,10 +189,10 @@ export class HttpRouter {
         router.get('/login', get(user => this.login(user)));
         router.post('/query', post((user, queryMessage: QueryMessage) => this.query(user, queryMessage)));
         router.post('/load', post((user, loadMessage: LoadMessage) => this.load(user, loadMessage)));
-        router.post('/save', post((user, saveMessage: SaveMessage) => this.save(user, saveMessage)));
+        router.post('/save', postCreate((user, saveMessage: SaveMessage) => this.save(user, saveMessage)));
 
         router.post('/read', postString((user, input: string) => this.read(user, input)));
-        router.post('/write', post((user, input: string) => this.write(user, input)));
+        router.post('/write', postStringCreate((user, input: string) => this.write(user, input)));
         router.post('/feeds', post((user, input: string) => this.feeds(user, input)));
 
         this.handler = router;
@@ -167,10 +226,9 @@ export class HttpRouter {
         };
     }
 
-    private async save(user: RequestUser, saveMessage: SaveMessage) : Promise<SaveResponse> {
+    private async save(user: RequestUser, saveMessage: SaveMessage) : Promise<void> {
         const userIdentity = serializeUserIdentity(user);
         await this.authorization.save(userIdentity, saveMessage.facts);
-        return {};
     }
 
     private async read(user: RequestUser, input: string): Promise<any[]> {
@@ -186,7 +244,7 @@ export class HttpRouter {
         return results;
     }
 
-    private async write(user: RequestUser, input: string): Promise<SaveResponse> {
+    private async write(user: RequestUser, input: string): Promise<void> {
         const knownFacts = await this.getKnownFacts(user);
         const parser = new SpecificationParser(input);
         parser.skipWhitespace();
@@ -202,7 +260,6 @@ export class HttpRouter {
 
         const userIdentity = serializeUserIdentity(user);
         await this.authorization.save(userIdentity, factRecords);
-        return {};
     }
 
     private async feeds(user: RequestUser, input: string): Promise<FeedsResponse> {
