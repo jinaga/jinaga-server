58a12c9641365aeaebce5a57afa397192c508903
Jan Verhaegen <jan.verhaegen@tss.be>
2022-11-11 21:10:45 +0100

    Facilitate debugging
    To debug:
    - Set breakpoints in the typescript sourcecode
    - Open a "Javascript Debug Terminal"
    - Run from there "npm run debug"
    - Use Postman to "post" facts to http://localhost:8080/jinaga/write or to "post" specifications to  http://localhost:8080/jinaga/read .   The debugger will stop when it reaches a breakpoint.

---
+++ src/debug.ts
@@ -0,0 +1,32 @@
+//import cors from "cors";
+import express from "express";
+import http from "http";
+import { JinagaServer } from "./jinaga-server";
+import process from "process";
+
+process.on('SIGINT', () => {
+  console.log("\n\nStopping replicator\n");
+  process.exit(0);
+});
+
+const app = express();
+const server = http.createServer(app);
+
+app.set('port', process.env.PORT || 8080);
+app.use(express.json());
+app.use(express.text());
+//app.use(cors());
+
+const pgConnection = process.env.JINAGA_POSTGRESQL ||
+  'postgresql://appuser:apppw@localhost:5432/appdb';
+  //'postgresql://repl:replpw@localhost:5432/replicator';
+const { handler } = JinagaServer.create({
+  pgStore: pgConnection
+});
+
+app.use('/jinaga', handler);
+
+server.listen(app.get('port'), () => {
+  console.log(`  Replicator is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`);
+  console.log('  Press CTRL-C to stop\n');
+});
