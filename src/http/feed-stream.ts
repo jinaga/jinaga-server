35ba41c5277ce6c436b0de6545d2e37f4e237ae7
Michael L Perry <michael@qedcode.com>
2023-12-02 08:18:14 -0600

    Improve memory management and concurrency

--- src/http/feed-stream.ts
+++ src/http/feed-stream.ts
@@ -2,36 +2,34 @@ import { FeedResponse } from "jinaga";
 import { Stream } from "./stream";
 
 export class FeedStream implements Stream<FeedResponse> {
-    private _queue: FeedResponse[] = [];
-    private _handlers: ((data: FeedResponse) => void)[] = [];
-    private _doneHandlers: (() => void)[] = [];
-    private _closed = false;
+    private queue: FeedResponse[] = [];
+    private handlers: ((data: FeedResponse) => void)[] = [];
+    private doneHandlers: (() => void)[] = [];
+    private closed = false;
 
     next(handler: (data: FeedResponse) => void): FeedStream {
-        this._handlers.push(handler);
-        this._queue.forEach(data => handler(data));
+        this.handlers.push(handler);
+        this.queue.forEach(data => handler(data));
         return this;
     }
 
     done(handler: () => void): FeedStream {
-        this._doneHandlers.push(handler);
+        this.doneHandlers.push(handler);
         return this;
     }
 
     close(): void {
-        this._closed = true;
-        this.end();
+        this.closed = true;
+        this.doneHandlers.forEach(handler => handler());
+        this.queue = [];
+        this.handlers = [];
+        this.doneHandlers = [];
     }
 
     feed(data: any): void {
-        if (this._closed) {
-            throw new Error("Cannot feed a closed stream");
+        if (!this.closed) {
+            this.queue.push(data);
+            this.handlers.forEach(handler => handler(data));
         }
-        this._queue.push(data);
-        this._handlers.forEach(handler => handler(data));
-    }
-
-    end(): void {
-        this._doneHandlers.forEach(handler => handler());
     }
 }
