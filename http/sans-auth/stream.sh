9560dbe5ea6b04b5f1dde1f516157aab1cca7af8
Michael L Perry <michael@qedcode.com>
2023-12-05 20:02:17 -0600

    Test streaming locally

--- http/sans-auth/stream.sh
+++ http/sans-auth/stream.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-curl -H "Accept: application/x-jinaga-feed-stream" http://localhost:8080/jinaga/feeds/wgo9gjZ-k1EJLgMyS4bZPaEaN2AzK-QUIFIFRvq52ZrNyraJoKKMiediCcbsEfR1F-UBEvyj-Waoe2wxyFsyGg
+curl -H "Accept: application/x-jinaga-feed-stream" http://localhost:8080/jinaga/feeds/jEYBSRk5DOr5Bedi1XGF17gAQoL_8z-rFbmD6CAu9Hux5N1doYTJLvkJPzY6l0JE1Uk1V6J-599LkQkYK4MQFA
