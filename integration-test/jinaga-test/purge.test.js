d90fe840282eacbb9acc9e4ca58487d9dc8ae4de
Michael L Perry <michael@qedcode.com>
2024-11-27 07:06:14 -0600

    Integration test the purge method

--- integration-test/jinaga-test/purge.test.js
+++ integration-test/jinaga-test/purge.test.js
@@ -7,6 +7,79 @@ const connectionString = `postgresql://dev:devpw@${host}:5432/integrationtest`;
 
 const model = createModel();
 
+describe("After-the-fact purge", () => {
+    let jInit, jPurge;
+    let closeInit, closePurge;
+
+    beforeEach(() => {
+        ({ j: jInit, close: closeInit } = JinagaServer.create({
+            pgKeystore: connectionString,
+            pgStore:    connectionString
+        }));
+
+        ({ j: jPurge, close: closePurge } = JinagaServer.create({
+            pgKeystore: connectionString,
+            pgStore:    connectionString,
+            purgeConditions: p => p
+                .whenExists(model.given(Order).match((order, facts) => facts.ofType(OrderCancelled)
+                    .join(orderCancelled => orderCancelled.order, order)
+                ))
+        }));
+    });
+
+    afterEach(async () => {
+        await closeInit();
+        await closePurge();
+    });
+
+    it("Should find descendants if purge condition is not met", async () => {
+        const store = await jInit.fact(new Store("storeId"));
+        const order = await jInit.fact(new Order(store, new Date()));
+        const item1 = await jInit.fact(new Item(order, new Product(store, "product1"), 1));
+        const item2 = await jInit.fact(new Item(order, new Product(store, "product2"), 1));
+
+        const itemsInOrder = model.given(Order).match(order =>
+            order.successors(Item, item => item.order)
+        );
+
+        await jPurge.purge();
+        const items = await jPurge.query(itemsInOrder, order);
+        expect(items).toEqual([item1, item2]);
+    });
+
+    it("Should purge descendants when condition is met", async () => {
+        const store = await jInit.fact(new Store("storeId"));
+        const order = await jInit.fact(new Order(store, new Date()));
+        const item1 = await jInit.fact(new Item(order, new Product(store, "product1"), 1));
+        const item2 = await jInit.fact(new Item(order, new Product(store, "product2"), 1));
+        const orderCancelled = await jInit.fact(new OrderCancelled(order, new Date()));
+
+        const itemsInOrder = model.given(Order).match(order =>
+            order.successors(Item, item => item.order)
+        );
+
+        await jPurge.purge();
+        const items = await jPurge.query(itemsInOrder, order);
+        expect(items).toEqual([]);
+    });
+
+    it("Should not purge the trigger fact", async () => {
+        const store = await jInit.fact(new Store("storeId"));
+        const order = await jInit.fact(new Order(store, new Date()));
+        const item1 = await jInit.fact(new Item(order, new Product(store, "product1"), 1));
+        const item2 = await jInit.fact(new Item(order, new Product(store, "product2"), 1));
+        const orderCancelled = await jInit.fact(new OrderCancelled(order, new Date()));
+
+        const cancelOfOrder = model.given(Order).match(order =>
+            order.successors(OrderCancelled, cancelled => cancelled.order)
+        );
+
+        await jPurge.purge();
+        const cancels = await jPurge.query(cancelOfOrder, order);
+        expect(cancels).toEqual([orderCancelled]);
+    });
+});
+
 describe("Real-time purge", () => {
     let j;
     let close;
@@ -67,6 +140,49 @@ describe("Real-time purge", () => {
         );
 
         const cancels = await j.query(cancelOfOrder, order);
+        expect(cancels).toEqual([orderCancelled]);
+    });
+});
+
+describe("After-the-fact purge with deep trigger", () => {
+    let jInit, jPurge;
+    let closeInit, closePurge;
+
+    beforeEach(() => {
+        ({ j: jInit, close: closeInit } = JinagaServer.create({
+            pgKeystore: connectionString,
+            pgStore:    connectionString
+        }));
+
+        ({ j: jPurge, close: closePurge } = JinagaServer.create({
+            pgKeystore: connectionString,
+            pgStore:    connectionString,
+            purgeConditions: p => p
+                .whenExists(model.given(Order).match((order, facts) => facts.ofType(OrderCancelledReason)
+                    .join(orderCancelledReason => orderCancelledReason.orderCancelled.order, order)
+                ))
+        }));
+    });
+
+    afterEach(async () => {
+        await closeInit();
+        await closePurge();
+    });
+
+    it("Should not purge ancestors of the trigger fact", async () => {
+        const store = await jInit.fact(new Store("storeId"));
+        const order = await jInit.fact(new Order(store, new Date()));
+        const item1 = await jInit.fact(new Item(order, new Product(store, "product1"), 1));
+        const item2 = await jInit.fact(new Item(order, new Product(store, "product2"), 1));
+        const orderCancelled = await jInit.fact(new OrderCancelled(order, new Date()));
+        const reason = await jInit.fact(new OrderCancelledReason(orderCancelled, "reason"));
+
+        const cancelOfOrder = model.given(Order).match(order =>
+            order.successors(OrderCancelled, cancelled => cancelled.order)
+        );
+
+        await jPurge.purge();
+        const cancels = await jPurge.query(cancelOfOrder, order);
         expect(cancels).toEqual([orderCancelled]);
     });
 });
