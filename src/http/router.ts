5d206426863247d7d4bd15c6c04dced3af0e2d25
Michael L Perry <michael@qedcode.com>
2025-10-18 17:21:03 -0500

    Restored recursive result extraction

--- src/http/router.ts
+++ src/http/router.ts
@@ -495,8 +495,9 @@ export class HttpRouter {
             const userIdentity = serializeUserIdentity(user);
             const start = this.selectStart(specification, declaration);
             const results = await this.authorization.read(userIdentity, start, specification);
-            Trace.counter("facts_read", results.length);
-            const resultStream = arrayToResultStream(results.map(r => r.result));
+            const extracted = extractResults(results);
+            Trace.counter("facts_read", extracted.count);
+            const resultStream = arrayToResultStream(extracted.result);
 
             return {
                 resultStream,
@@ -976,6 +977,28 @@ function parseString(input: any): string {
         throw new Invalid("Expected a string. Check the content type of the request.");
     }
     return input;
+}
+
+function extractResults(obj: any): { result: any, count: number } {
+    if (Array.isArray(obj)) {
+        const projectedResults: ProjectedResult[] = obj;
+        const results = projectedResults.map(r => extractResults(r.result));
+        const count = results.reduce((acc, res) => acc + res.count + 1, 0);
+        return { result: results.map(r => r.result), count };
+    }
+    else if (typeof obj === "object") {
+        const keys = Object.keys(obj);
+        const results = keys.reduce((acc, key) => {
+            const extracted = extractResults(obj[key]);
+            acc.result[key] = extracted.result;
+            acc.count += extracted.count;
+            return acc;
+        }, { result: {} as any, count: 0 });
+        return results;
+    }
+    else {
+        return { result: obj, count: 0 };
+    }
 }
 
 function handleError(error: any, req: Request, res: Response, next: NextFunction) {
